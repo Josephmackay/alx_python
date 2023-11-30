fa628566fe83aa614c2b7b064653d6abf464559a
Josephmackay <lindaansahqwe@gmail.com>
2023-11-30 19:25:36 +0000

    e

---
+++ python-functions/6-password.py
@@ -0,0 +1,17 @@
+def validate_password(password):
+    if len(password) < 8:
+        return False
+   
+    if not re.search("[a-z]", password):
+        return False
+    
+    if not re.search("[A-Z]", password):
+        return False
+ 
+    if not re.search("[0-9]", password):
+        return False
+ 
+    if re.search(" ", password):
+        return False
+
+    return True
