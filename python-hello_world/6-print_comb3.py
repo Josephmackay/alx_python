5db29410aec87bcccbccca02255073cae24acc8b
Josephmackay <150284151+Josephmackay@users.noreply.github.com>
2023-12-01 10:07:32 +0000

    k

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
-        print(f"{first_digit}{second_digit}, ", end = '')
+        print("{}{}, ".format(first_digit, second_digit), end = '')
