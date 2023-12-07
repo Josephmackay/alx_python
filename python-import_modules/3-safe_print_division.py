56477aa583509885cbf1eaaaaa2d85ce53d35fa8
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 18:36:51 +0000

    g

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,7 +2,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero")
+        print("Inside result: None")
         return None
     finally:
         print("Inside result: {}".format(result))
