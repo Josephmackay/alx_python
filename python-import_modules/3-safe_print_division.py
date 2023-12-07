cab65204df646dad6d54f092d1a6f0a22fede5ec
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 19:05:53 +0000

    ij

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,9 +2,9 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
-        print("{} = None".format(result))
+        print("Error: Division by zero")
         return None
     finally:
         print("Inside result: {}".format(result))
     return result
+
