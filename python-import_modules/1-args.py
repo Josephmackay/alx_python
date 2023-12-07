ae217985514079c3be081644ac42e0ca06ff21f0
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 18:22:03 +0000

    ijk

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,12 +8,21 @@ elif number_of_arguments == 2:
     print("1 argument:")
     print("1: {}".format(sys.argv[1]))
 elif number_of_arguments == 3:
-    # Handle three argument case
     print("2 arguments:")
     print("1: {}".format(sys.argv[1]))
     print("2: {}".format(sys.argv[2]))
+elif number_of_arguments == 4:
+    print("3 arguments:")
+    print("1: {}".format(sys.argv[1]))
+    print("2: {}".format(sys.argv[2]))
+    print("3: {}".format(sys.argv[3]))
+elif number_of_arguments == 5:
+    print("4 arguments:")
+    print("1: {}".format(sys.argv[1]))
+    print("2: {}".format(sys.argv[2]))
+    print("3: {}".format(sys.argv[3]))
+    print("4: {}".format(sys.argv[4]))
 else:
     print("Unexpected number of arguments: {}".format(number_of_arguments))
-    # Handle unexpected arguments
 if __name__ == "__main__":
     pass        
