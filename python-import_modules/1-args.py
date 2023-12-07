771a81231439b5434e69c82cf97f9dc5fa0bd166
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 17:58:40 +0000

    h

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,11 +3,15 @@ import sys
 number_of_arguments = len(sys.argv)
 
 if number_of_arguments == 1:
+    # Handle single argument case
     print("Number of argument(s): 0.")
+elif number_of_arguments == 2:
+    # Handle two argument case
+    print(f"Number of argument: {number_of_arguments}.")
+    print(f"1: {sys.argv[1]}")
+    print(f"2: {sys.argv[2]}")
 else:
-    print(f"Number of argument{'s' if number_of_arguments > 1 else ''}: {number_of_arguments}.")
-
-    for i, argument in enumerate(sys.argv[1:]):
-        print(f"{i + 1}: {argument}")
+    print(f"Unexpected number of arguments: {number_of_arguments}")
+    # Handle unexpected arguments
 if __name__ == "__main__":
     pass        
