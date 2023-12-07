707254dde38db5e65fb121b0d65df48c9a4422a2
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 19:10:51 +0000

    hh

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,19 @@
 def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Error: Division by zero")
-        return None
-    finally:
-        print("Inside result: {}".format(result))
-    return result
+  """
+  Divides two integers and prints the result safely.
 
+  Args:
+    a: The first integer to be divided.
+    b: The second integer to be divided by.
+
+  Returns:
+    The result of the division or None if an error occurs.
+  """
+  try:
+    result = a / b
+    print(f"Inside result: {result}")
+  except ZeroDivisionError:
+    print("Error: Division by zero")
+    return None
+  finally:
+    return result
