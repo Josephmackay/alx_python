5521576894b5968560f19bf97f09ff84d93302d0
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 18:10:36 +0000

    12

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,19 +3,20 @@ import sys
 number_of_arguments = len(sys.argv)
 
 if number_of_arguments == 1:
-    print("Number of argument(s): 0.")
+    print("1 argument")
+    print("1: {}".format(sys.argv[1]))
 elif number_of_arguments == 2:
     print("2 arguments:")
-    print(f"1: {sys.argv[1]}")
-    print(f"2: {sys.argv[2]}")
+    print("1: {}".format(sys.argv[1]))
+    print("2: {}".format(sys.argv[2]))
 elif number_of_arguments == 3:
     # Handle three argument case
     print("3 arguments:")
-    print(f"1: {sys.argv[1]}")
-    print(f"2: {sys.argv[2]}")
-    print(f"3: {sys.argv[3]}")
+    print("1: {}".format(sys.argv[1]))
+    print("2: {}".format(sys.argv[2]))
+    print("3: {}".format(sys.argv[3]))
 else:
-    print(f"Unexpected number of arguments: {number_of_arguments}")
+    print("Unexpected number of arguments: {}".format(number_of_arguments))
     # Handle unexpected arguments
 if __name__ == "__main__":
     pass        
