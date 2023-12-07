217037b4735e7035104cca8c1866bf96102472cb
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 17:51:55 +0000

    1

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,11 @@
+import sys
+
+number_of_arguments = len(sys.argv)
+
+if number_of_arguments == 1:
+    print("Number of argument(s): 0.")
+else:
+    print(f"Number of argument{'s' if number_of_arguments > 1 else ''}: {number_of_arguments}.")
+
+    for i, argument in enumerate(sys.argv[1:]):
+        print(f"{i + 1}: {argument}")
