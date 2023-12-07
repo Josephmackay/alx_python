8f2f34c4f01d10fb5e25cee542a05e93f8394b44
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 18:39:48 +0000

    trtr

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,7 +1,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisionError:
+    except b == 0:
         print("Inside result: None")
         return None
     finally:
