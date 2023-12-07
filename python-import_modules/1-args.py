400bfdf8c1d95d97b425ec01eba3c94c576ced23
Josephmackay <lindaansahqwe@gmail.com>
2023-12-07 18:14:25 +0000

    y

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,7 +8,6 @@ if number_of_arguments == 1:
 elif number_of_arguments == 2:
     print("1 argument:")
     print("1: {}".format(sys.argv[1]))
-    print("2: {}".format(sys.argv[2]))
 elif number_of_arguments == 3:
     # Handle three argument case
     print("3 arguments:")
