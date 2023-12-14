e0e532e242a2458be5727b4baebefa48ad349819
Josephmackay <lindaansahqwe@gmail.com>
2023-12-14 18:15:03 +0000

    yhyu

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -58,16 +58,3 @@ try:
 except Exception as e:
     print(e)
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
