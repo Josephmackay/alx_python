8b0c441c9ef845fa5cb8790360358c3cbfe6d766
Josephmackay <lindaansahqwe@gmail.com>
2023-12-14 18:10:30 +0000

    w

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,3 +1,22 @@
+"""
+This function checks if an object is an exact instance of a specified class without importing any external modules.
+
+Args:
+    obj: The object to be checked.
+    a_class: The class to compare against.
+
+Returns:
+    True if the object is an exact instance of the class, False otherwise.
+
+Examples:
+    ```python
+    is_same_class(1, int)  # True
+    is_same_class([], list)  # True
+    is_same_class("string", str)  # True
+    is_same_class(1.0, float)  # False (not exact class match)
+    is_same_class([], tuple)  # False (subclass, not exact match)
+    ```
+"""
 class Square:
     def __init__(self, size=0):
         self.__size = size  # Initialize private attribute
