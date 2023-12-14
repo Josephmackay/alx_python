e5947ea17962fe788a393e2952a3be054c461e56
Josephmackay <lindaansahqwe@gmail.com>
2023-12-14 11:32:51 +0000

    hh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,8 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    def size(self):
-        return self.__size
+    def size(self, new_size):
+        if isinstance(new_size, int) and new_size > 0:
+            self.__size = new_size
+
+    ...
