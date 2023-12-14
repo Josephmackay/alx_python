ed39625b1cf5d8d45a6cbf5976719023a286f76e
Josephmackay <lindaansahqwe@gmail.com>
2023-12-14 12:33:07 +0000

    TF

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,15 +1,9 @@
 def is_same_class(obj, a_class):
     # Use isinstance check with strict comparison
-    return isinstance(obj, a_class) and obj.__class__ == a_class
+    if isinstance(obj, a_class) and obj.__class__ == a_class:
+        return True
+    else:
+        return False
 
-# Example usage
 a = 1
-if is_same_class(a, int):
-    print("{} is an exact instance of the class {}".format(a, int.__name__))
-if is_same_class(a, float):
-    print("{} is an exact instance of the class {}".format(a, float.__name__))
-if is_same_class(a, object):
-    print("{} is an exact instance of the class {}".format(a, object.__name__))
-    
-# Output
-print("{} is an exact instance of the class {}".format(a, int.__name__))
+print(is_same_class(a, int))
