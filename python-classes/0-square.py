45222b72827759c4aa3df2d6b476acb01e0b4ed4
Josephmackay <lindaansahqwe@gmail.com>
2023-12-14 17:56:53 +0000

    uu

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,19 @@
-class Square:
-    def __init__(self, size):
-        self.__size = size
+"""
+This module defines the is_same_class function for checking exact class matches.
 
-    def size(self, new_size):
-        if isinstance(new_size, int) and new_size > 0:
-            self.__size = new_size
+Why no imports:
+
+- Showcases a self-contained solution without external dependencies.
+
+Function:
+
+- is_same_class(obj, a_class): Returns True if obj is an exact instance of a_class, False otherwise.
+
+"""
+
+def is_same_class(obj, a_class):
+    return isinstance(obj, a_class) and obj.__class__ == a_class
+
+
 
     ...
