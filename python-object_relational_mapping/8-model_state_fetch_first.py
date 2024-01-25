5bf8bbc64704b7add1955cf0682d353e1f503e94
Josephmackay <150284151+Josephmackay@users.noreply.github.com>
2024-01-25 18:43:35 +0000

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,46 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+def fetch_first_state():
+    """
+    Fetch the first State object from the database and print it
+    """
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create the engine and bind it to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the database to get the first State object
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Print the result
+    if first_state:
+        print("{}: {}".format(first_state.id, first_state.name))
+    else:
+        print("Nothing")
+
+    # Close the session
+    session.close()
+
+if __name__ == "__main__":
+    fetch_first_state()
 
