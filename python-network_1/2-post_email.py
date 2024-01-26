121185b14e68e8f537bac0abe147ec9a382cef83
Josephmackay <150284151+Josephmackay@users.noreply.github.com>
2024-01-26 18:50:45 +0000

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,9 +5,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
-    data = {'email': email}
-    response = requests.post(url, data=data)
-    
-    print("Your email is:", email)
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
     print(response.text)
