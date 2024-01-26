37c5de9c7244e23c6d6367fb8a306f0e396e3712
Josephmackay <150284151+Josephmackay@users.noreply.github.com>
2024-01-26 18:36:40 +0000

    Create 5-json_api.py

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    data = {'q': q}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
